43ed021e1286ee72980b21f35c9ed47353c9cf35
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-08 19:15:22 +0100

    Implemento moduli DB per rotta Show - id movie + review movie

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -7,7 +7,6 @@ function index(req, res) {
 
     connection.query(sql, (err, movies) => {
         if (err) return res.status(500).json({ message: err.message }) // se il valore è null, restituisce errore
-
         res.json(movies) // altrimenti restituisce json con i movies
     })
 }
@@ -15,8 +14,35 @@ function index(req, res) {
 function show(req, res) {
     // funzione che mostrerà dettaglio movie + review
     // restituisce json
-    res.json({
-        message: 'Movies SHOW'
+    const id = parseInt(req.params.id) // recupero parametro
+
+    if (isNaN(id)) // se l'id è NaN, esce errore perchè non trova niente
+        return res.status(404).json({
+            error: 'Not Found',
+            message: 'Movie not found'
+        })
+
+    const sql = `SELECT * FROM movies WHERE id = ?`
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ message: err.message })
+        if (results.length === 0) return res.status(404).json({
+            error: 'Not Found',
+            message: 'Movie not found',
+        })
+
+        // se non ci sono errori e se l'id cercato è > di 0 allora il primo elemento dell'array è l'id che stiamo cercando
+        const movie = results[0]
+
+        const sql = `SELECT * FROM reviews WHERE movie_id = ?` // cerco le recensioni per id movie
+
+        connection.query(sql, [id], (err, results) => {
+            if (err) return res.status(500).json({ message: err.message })
+
+            movie.reviews = results // mostro i reviews inerenti a id del movie
+
+            res.json(movie) // mostro movie cercato tramite id e le sue relative reviews
+        })
     })
 }
 
