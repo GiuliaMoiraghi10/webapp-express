4ef31b9141f7e8a41d642e8c78df178d8830ec6b
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-08 17:47:57 +0100

    Modifica a variabili d'ambiente + connessione a mysql

--- data/db.js
+++ data/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host: DB_HOST || 'localhost',
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
 
 connection.connect((err) => {
