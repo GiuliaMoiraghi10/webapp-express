111c550db0b8d9f58038c722d909a42cb8c2e4aa
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-08 18:10:38 +0100

    Implemento moduli DB per rotta Index

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -3,8 +3,12 @@ const connection = require('../data/db')
 function index(req, res) {
     // funzione che mostrerà tutti i movies
     // restituisce un json
-    res.json({
-        message: 'Movies INDEX'
+    const sql = `SELECT * FROM movies`
+
+    connection.query(sql, (err, movies) => {
+        if (err) return res.status(500).json({ message: err.message }) // se il valore è null, restituisce errore
+
+        res.json(movies) // altrimenti restituisce json con i movies
     })
 }
 
