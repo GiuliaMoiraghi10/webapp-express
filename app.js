dd7c439c09e7c8b335c07091ce8a68690418dc6d
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-08 16:20:06 +0100

    Servo staticamente la cartella public e gestisco errori con middlewares

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const notFound = require('./middlewares/notFound')
+const errorsHandler = require('./middlewares/errorsHandler')
+
+// rendo visibile cartella public (con dentro img)
+app.use(express.static('public'))
+
+
+// creo rotta /
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
 
+
+// Middlewares --> DOPO rotta!
+app.use(errorsHandler)
+app.use(notFound)
+
+
+// metto in ascolto la porta
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
