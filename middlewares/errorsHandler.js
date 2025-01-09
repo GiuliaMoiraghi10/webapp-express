2545318a8fbdb4a986440d2b17a14fadf01bef6c
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-01-09 09:38:53 +0100

    Modifica middleware - errorsHandler

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,4 +1,4 @@
-function errorsHandler(err, req, res) {
+function errorsHandler(err, _, res, _) { // deve avere 4 parametri per forza (anche req e next anche se non si usano)
     res.status(500).json({
         message: err.message,
     })
